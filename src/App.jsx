e86c634be52fe85b7a7eb3719212b398b58edd5c
khasanov_ibroxim <xibroxim11@gmail.com>
2025-03-06 21:29:33 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, useLocation } from "react-router-dom";
 import { ROUTERS } from "./utils/consts.jsx";
 import Navbar from "./component/navbar/navbar.jsx";
 import Footer from "./component/footer/footer.jsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Loading from "@/component/loading/loading.jsx";
 
 function App() {
     const location = useLocation();
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setLoading(true);
-
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 1000);
-
-        return () => clearTimeout(timer);
-    }, [location]);
-
-    useEffect(() => {
-        if (!loading) {
-            AOS.init({ once: false, duration: 1000 }); // AOS faqat yuklanish tugagandan keyin ishga tushadi
-        }
-    }, [loading]);
+        AOS.init({ once: false });
+        AOS.refresh();
+    }, [location.pathname]); // Har safar sahifa o'zgarganda AOS qayta yuklanadi
 
     return (
-        <>
-            {loading ? (
-                <Loading />
-            ) : (
-                <Routes>
-                    {ROUTERS.map(({ Path, Component }, index) => (
-                        <Route key={index} path={Path} element={
-                            <>
-                                <Navbar />
-                                <Component />
-                                <Footer />
-                            </>
-                        } />
-                    ))}
-                </Routes>
-            )}
-        </>
+        <Routes>
+            {ROUTERS.map(({ Path, Component }, index) => (
+                <Route key={index} path={Path} element={
+                    <>
+                        <Navbar />
+                        <Component />
+                        <Footer />
+                    </>
+                } />
+            ))}
+        </Routes>
     );
 }
 
