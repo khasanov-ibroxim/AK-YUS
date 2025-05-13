f5ec6fe213849c871c2061ac95a2e51976f02f3c
khasanov_ibroxim <xibroxim11@gmail.com>
2025-05-14 00:22:33 +0500

    14

--- src/utils/db_products.jsx
+++ src/utils/db_products.jsx
@@ -10,15 +10,15 @@ import s2_item_3_detail from "@/assets/allphoto/5V1A0532_resized.jpg"
 import s2_item_4_detail from "@/assets/allphoto/5V1A9797_resized.jpg"
 
 
-import s4_item_1 from "@/assets/allphoto/5V1A0138_resized.jpg"
-import s4_item_2 from "@/assets/allphoto/5V1A0580_resized.jpg"
-import s4_item_3 from "@/assets/allphoto/5V1A0611_resized.jpg"
-import s4_item_4 from "@/assets/allphoto/5V1A9803_resized.jpg"
+import s4_item_1 from "@/assets/allphoto/5V1A0321_resized.jpg"
+import s4_item_2 from "@/assets/allphoto/5V1A0337_resized_v1.jpg"
+import s4_item_3 from "@/assets/allphoto/5V1A0475_resized.jpg"
+import s4_item_4 from "@/assets/allphoto/5V1A0543_resized.jpg"
 
-import s4_item_1_detail from "@/assets/allphoto/5V1A0167_resized.jpg"
-import s4_item_2_detail from "@/assets/allphoto/5V1A0590_resized.jpg"
-import s4_item_3_detail from "@/assets/allphoto/5V1A0620_resized.jpg"
-import s4_item_4_detail from "@/assets/allphoto/5V1A9837_resized.jpg"
+import s4_item_1_detail from "@/assets/allphoto/5V1A0301_resized.jpg"
+import s4_item_2_detail from "@/assets/allphoto/5V1A0331_resized.jpg"
+import s4_item_3_detail from "@/assets/allphoto/5V1A0468_resized.jpg"
+import s4_item_4_detail from "@/assets/allphoto/5V1A0532_resized.jpg"
 
 
 export const Category = [
@@ -58,25 +58,27 @@ export const BestSell = [
         cat_id: 0,
         name: "Short sleeve mushrooms T-shirt 1",
         price: "$27.90",
-        images: [s2_item_1_detail , s2_item_1],
-    }, {
+        images: s2_item_1_detail,
+    },
+    {
         id: 1,
         cat_id: 0,
         name: "Short sleeve mushrooms T-shirt 2",
         price: "$27.90",
-        images: [s2_item_2_detail,s2_item_2],
-    }, {
+        images: s2_item_2_detail,
+    },
+    {
         id: 2,
         cat_id: 0,
         name: "Short sleeve mushroomssad T-shirt 3",
         price: "$27.90",
-        images: [s2_item_3_detail,s2_item_3],
+        images: s2_item_3_detail,
     }, {
         id: 3,
         cat_id: 0,
         name: "Short sleeve mushrooms T-shirt 4",
         price: "$27.90",
-        images: [s2_item_4_detail,s2_item_4],
+        images: s2_item_4_detail,
     }, {
         id: 4,
         cat_id: 0,
@@ -90,25 +92,25 @@ export const Arrivals = [
     {
         id: 0,
         cat_id: 0,
-        name: "У нас всегда актуальные новинки",
+        name: "Одежда, которая подчёркивает вашу индивидуальность.",
         price: "$27.90",
         images: [s4_item_1_detail, s4_item_1],
     }, {
         id: 1,
         cat_id: 0,
-        name: "Стиль для любого случая — от повседневного до торжественного",
+        name: "Следите за трендами вместе с нами",
         price: "$27.90",
         images: [s4_item_2_detail, s4_item_2],
     }, {
         id: 2,
         cat_id: 0,
-        name: "Быстрая доставка и удобные способы оплаты",
+        name: "Комфорт и качество в каждой детали.",
         price: "$27.90",
         images: [s4_item_3_detail, s4_item_3],
     }, {
         id: 3,
         cat_id: 0,
-        name: "Идеальный образ с персональными рекомендациями",
+        name: "Подберите идеальный лук за пару кликов — просто и удобно.",
         price: "$27.90",
         images: [s4_item_4_detail, s4_item_4],
     },
