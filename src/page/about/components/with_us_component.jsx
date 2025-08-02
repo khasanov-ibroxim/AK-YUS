2b106b31eccc0592d00c27f68c21395cbc7bcf8b
khasanov_ibroxim <xibroxim11@gmail.com>
2025-08-02 17:22:14 +0500

    02

--- src/page/about/components/with_us_component.jsx
+++ src/page/about/components/with_us_component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {CONTACT} from "@/utils/consts.jsx";
+import {useTranslation} from "react-i18next";
 
 const WithUsComponent = () => {
+    const {t} = useTranslation();
     return (
         <div className={"with-us"}>
-            <h1>Мы всегда на связи</h1>
+            <h1>{t("about.contact.title")}</h1>
             <ul>
-                <li>Мы открыты для делового сотрудничества и новых предложений.</li>
-                <li>Наша команда всегда на связи и готова к диалогу.</li>
-                <li>Выберите любой удобный <Link to={CONTACT}>способ связи</Link>  — мы ответим в кратчайшие сроки.</li>
+                <li>{t("about.contact.i1")}</li>
+                <li>{t("about.contact.i2")}</li>
+                <li dangerouslySetInnerHTML={{__html:t("about.contact.i3")}}></li>
             </ul>
         </div>
     );
